81086bdcff3676b21ac7e328e244a8050d4a43f1
Henry Gillard <henry@MacBook-Pro.local>
2021-06-07 12:55:36 -0700

    render deal button

--- js/main.js
+++ js/main.js
@@ -1,31 +1,65 @@
-/*----- constants -----*/
-const MASTER_DECK = [2,3,4,5,6,7,8,9,10,10,10,10,11];
-const PLAYER_HAND = [];
-const DEALER_HAND = [];
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
 
 /*----- app's state (variables) -----*/
-let playerTotal;
-let dealerTotal;
+let playerHand;
+let dealerHand;
 let dealerSum;
 let playerSum;
-let gameStatus;
+let gameStatus
 let winner;
+let betPower;
+let bet;
 
 /*----- cached element references -----*/
 const dealEl = document.querySelector("#deal");
 const headerEl = document.querySelector("h1");
-
+const playerCardEl = document.querySelector(".player-card");
+const playerSumEl = document.getElementById("player-sum");
+const hitEl = document.querySelector("#hit");
+const standEl = document.querySelector("#stand");
 /*----- event listeners -----*/
-
+document.querySelector("#deal").addEventListener("click", setDeal)
 
 /*----- functions -----*/
-function init() {
-    headerEl.style.color = "yellow";
-} 
 init()
+function init() {
+   
+  playerHand = [];
+  dealerHand = [];
+  playerSum = 0;
+  dealerSum = 0;
+  bet = 0;
+  betPower = 0;
+  gameStatus = null;
+  winner = null;
+  //set hit and stand buttons to invisible//
+ hitEl.disabled = true;
+  standEl.disabled = true;
+   render();
+} 
 
-function pickACard() {
- let randomCard = MASTER_DECK[Math.floor(Math.random() * MASTER_DECK.length)]
-  console.log(randomCard);
+
+function setDeal() {
+  standEl.disabled = false;
+  hitEl.disabled = false;
+  headerEl.innerHTML = "Do you want to hit or stand?"
+  computeHand();
+
+ }
+
+function computeHand() {
+  let randomCard = ranks[Math.floor(Math.random() * ranks.length)] // this could be in handleHand function
+  
+ // add the random integer to player hand array//
+
+playerCardEl.innerHTML = randomCard;
+  
+playerSumEl.innerHTML = randomCard; // add the next values
 }
-pickACard()
+function render() {
+    
+}
+
+
