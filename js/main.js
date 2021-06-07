d254d353dd3d9d6ad2d9ea35885b5848a8da5ec4
Henry Gillard <henry@MacBook-Pro.local>
2021-06-07 08:43:16 -0700

    Add pickACard function

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 /*----- constants -----*/
-
+const MASTER_DECK = [2,3,4,5,6,7,8,9,10,10,10,10,11];
+const PLAYER_HAND = [];
+const DEALER_HAND = [];
 
 /*----- app's state (variables) -----*/
-
+let playerTotal;
+let dealerTotal;
+let dealerSum;
+let playerSum;
+let gameStatus;
+let winner;
 
 /*----- cached element references -----*/
-
+const dealEl = document.querySelector("#deal");
+const headerEl = document.querySelector("h1");
 
 /*----- event listeners -----*/
 
 
 /*----- functions -----*/
+function init() {
+    headerEl.style.color = "yellow";
+} 
+init()
+
+function pickACard() {
+ let randomCard = MASTER_DECK[Math.floor(Math.random() * MASTER_DECK.length)]
+  console.log(randomCard);
+}
+pickACard()
