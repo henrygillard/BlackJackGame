9c02c32df58220288da522622e4bb6adedab45bc
Henry Gillard <henry@MacBook-Pro.local>
2021-06-07 15:04:56 -0700

    Card Value rendered

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const masterDeck = [];
 
 
 /*----- app's state (variables) -----*/
@@ -15,7 +16,8 @@ let bet;
 /*----- cached element references -----*/
 const dealEl = document.querySelector("#deal");
 const headerEl = document.querySelector("h1");
-const playerCardEl = document.querySelector(".player-card");
+const player1CardEl = document.getElementById("pcard1");
+const player2CardEl = document.getElementById("pcard2");
 const playerSumEl = document.getElementById("player-sum");
 const hitEl = document.querySelector("#hit");
 const standEl = document.querySelector("#stand");
@@ -44,22 +46,46 @@ function init() {
 function setDeal() {
   standEl.disabled = false;
   hitEl.disabled = false;
+  dealEl.disabled = true;
   headerEl.innerHTML = "Do you want to hit or stand?"
   computeHand();
 
  }
 
-function computeHand() {
-  let randomCard = ranks[Math.floor(Math.random() * ranks.length)] // this could be in handleHand function
+function computeHand(hand) {
   
- // add the random integer to player hand array//
-
-playerCardEl.innerHTML = randomCard;
+ // Iterate through the suits and ranks arrays //
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      masterDeck.push({ // Push those into masterDeck object array //
+        face: `${suit}${rank}`,
+        value: Number(rank) 
+      })
+    })
+  }) 
+  hand = masterDeck[Math.floor(Math.random() * masterDeck.length)] // set hand parameter to random face value
   
-playerSumEl.innerHTML = randomCard; // add the next values
+  player1CardEl.innerHTML = `${hand.face}` // Set card1 element to face value
+  player2CardEl.innerHTML = `${hand.face}` // Set card 2 element to face value
+  
+  console.log(masterDeck);
+  playerSum = hand.value + hand.value; // Set player sum to card 1 + card 2 value
+  playerSumEl.innerHTML = `${playerSum}`;
+ 
+  render();
+ 
+  // console.log(playerHand[1].value); This will give you the card value!
+  
+// playerSumEl.innerHTML = randomCard; // add the next values
 }
 function render() {
+  // console.log(masterDeck);
+//   let playerHand = 
+//   console.log(playerHand);
+ ;
+  
+ 
     
-}
+};
 
 
